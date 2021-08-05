3e3baa7717da77d3f47cfbb154c3f1131abc2e73
Nabeeha Nuba <nabeehanuba75@gmail.com>
2021-08-04 21:10:27 -0400

    changes to website

--- script.py
+++ script.py
@@ -8,9 +8,9 @@ def home():
     return render_template("home.html")
 
 
-@app.route('/about/')  # homepage
-def about():
-    return render_template("about.html")
+@app.route('/contact/')  # homepage
+def contact():
+    return render_template("contact.html")
 
 
 if __name__ == "__main__":
